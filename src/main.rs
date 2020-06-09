eec5d7906e3d281aafd5a3a9d7c158efa40d3f2c
Lee Ball <43632885+catleeball@users.noreply.github.com>
2020-06-09 14:06:07 -0700

    Update to use stable Clap (#3)
    I rebased some commits together badly and head of master now has a mix of both stable Clap and beta Clap. This is intended to reconcile the issue and use stable Clap.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,37 @@
+// extern clap
 use clap::{crate_version, App, Arg};
 use headless_chrome::{protocol::page::ScreenshotFormat, Browser};
 
 fn main() -> Result<(), failure::Error> {
-    let args: clap::ArgMatches = get_args();
+    let args = App::new("shotweb")
+        .about("Take screenshots of Chrome-rendered webpages.")
+        .version(crate_version!())
+        .arg(Arg::with_name("url")
+            .help("URL or file to take a screencap of. i.e. https://example.com or file:///path/to/file.html")
+            .index(1)
+            .required(true))
+        .arg(Arg::with_name("output-path")
+            .help("Local file path to save screenshot image to.")
+            .short("o")
+            .takes_value(true)
+            .default_value("/tmp/screenshot.png"))
+        .arg(Arg::with_name("element")
+            .help("CSS selector of element to screenshot.")
+            .short("e")
+            .takes_value(true))
+        .arg(Arg::with_name("image-format")
+            .help("Local file path to save screenshot image to.")
+            .short("i")
+            .takes_value(true)
+            .default_value("png")
+            .possible_values(&["png", "jpg"]))
+        .get_matches();
     let url: &str = args.value_of("url").unwrap();
     let output_path: &str = args.value_of("output-path").unwrap();
 
     let img = screenshot_webpage(url)?;
     std::fs::write(output_path, img)?;
     Ok(())
-}
-
-fn get_args() -> clap::ArgMatches {
-    return App::new("shotweb")
-        .about("Take screenshots of Chrome-rendered webpages.")
-        .version(crate_version!())
-        .arg(Arg::new("url")
-            .about("URL or file to take a screencap of. i.e. https://example.com or file:///path/to/file.html")
-            .index(1)
-            .required(true))
-        .arg(Arg::new("output-path")
-            .about("Local file path to save screenshot image to.")
-            .short('o')
-            .takes_value(true)
-            .default_value("/tmp/screenshot.png")
-            .index(1))
-        .arg(Arg::new("element")
-            .about("CSS selector of element to screenshot.")
-            .short('e')
-            .takes_value(true))
-        .arg(Arg::new("image-format")
-            .about("Local file path to save screenshot image to.")
-            .short('i')
-            .takes_value(true)
-            .default_value("png")
-            .possible_values(&["png", "jpg"]))
-        .get_matches();
 }
 
 /// Fullscreen screenshot of entire surface of given URL rendered in Chrome.
