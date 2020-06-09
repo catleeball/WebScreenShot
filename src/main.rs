b0c290db4e3958b4f5c82621dedb4b80b7051a51
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-06-08 21:03:48 -0700

    Add CLI arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
+use clap::{crate_version, App, Arg};
 use headless_chrome::{protocol::page::ScreenshotFormat, Browser};
 
-
 fn main() -> Result<(), failure::Error> {
-    let url: &str = "file:////Users/int/src/tmnt_wikipedia_bot/assets/html/tmnt.html";
-    let output_path: &str = "/tmp/logo.png";
+    let args: clap::ArgMatches = get_args();
+    let url: &str = args.value_of("url").unwrap();
+    let output_path: &str = args.value_of("output-path").unwrap();
 
     let img = screenshot_webpage(url)?;
     std::fs::write(output_path, img)?;
     Ok(())
+}
+
+fn get_args() -> clap::ArgMatches {
+    return App::new("shotweb")
+        .about("Take screenshots of Chrome-rendered webpages.")
+        .version(crate_version!())
+        .arg(Arg::new("url")
+            .about("URL or file to take a screencap of. i.e. https://example.com or file:///path/to/file.html")
+            .index(1)
+            .required(true))
+        .arg(Arg::new("output-path")
+            .about("Local file path to save screenshot image to.")
+            .short('o')
+            .takes_value(true)
+            .default_value("/tmp/screenshot.png")
+            .index(1))
+        .arg(Arg::new("element")
+            .about("CSS selector of element to screenshot.")
+            .short('e')
+            .takes_value(true))
+        .arg(Arg::new("image-format")
+            .about("Local file path to save screenshot image to.")
+            .short('i')
+            .takes_value(true)
+            .default_value("png")
+            .possible_values(&["png", "jpg"]))
+        .get_matches();
 }
 
 /// Fullscreen screenshot of entire surface of given URL rendered in Chrome.
@@ -20,4 +48,3 @@ fn screenshot_webpage(url: &str) -> Result<Vec<u8>, failure::Error> {
             .capture_screenshot(ScreenshotFormat::PNG, None, true)?
         );
 }
-
